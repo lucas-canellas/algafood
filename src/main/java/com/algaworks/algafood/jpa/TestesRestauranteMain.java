3d8cec9275002b6c2da952338f03a2429d379530
lucas <lucascanellasdev@gmail.com>
2022-09-22 12:45:20 -0300

    3.16. Desafio: Lombok e repositório de restaurantes

---
+++ src/main/java/com/algaworks/algafood/jpa/TestesRestauranteMain.java
@@ -0,0 +1,54 @@
+package com.algaworks.algafood.jpa;
+
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+public class TestesRestauranteMain {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		RestauranteRepository restaurantes = applicationContext.getBean(RestauranteRepository.class);
+		
+		Restaurante restaurante1 =  new Restaurante();
+		restaurante1.setNome("Bobs");
+		restaurante1.setTaxaFrete(new BigDecimal(10));
+		
+		Restaurante restaurante2 = new Restaurante();
+		restaurante2.setNome("Habibs");
+		restaurante2.setTaxaFrete(new BigDecimal(20));
+		
+		restaurante1 = restaurantes.adicionar(restaurante1);
+		restaurantes.adicionar(restaurante2);
+		
+		restaurante1.setNome("Vasco");
+		restaurantes.adicionar(restaurante1);
+		
+		var testeBusca = restaurantes.porId(1L);
+		System.out.println(testeBusca.getNome());
+		
+		List<Restaurante> todosRestaurantes = restaurantes.todas(); 
+		
+		for(Restaurante restaurante : todosRestaurantes) {
+			System.out.println(restaurante.getNome());
+		}
+		
+		restaurantes.remover(restaurante1);
+		
+		
+		
+		
+	}
+
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
+
+insert into restaurante (nome, taxa_frete) values ('La Non Sei', 20);
+insert into restaurante (nome, taxa_frete) values ('La Falta de Criatividade', 10);
