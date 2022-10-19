265794fa3574516ebd7b26701f18cabf4f4071ed
lucas <lucascanellasdev@gmail.com>
2022-10-19 15:35:55 -0300

    6.7. Desafio: mapeando relacionamento muitos-para-um

--- src/main/java/com/algaworks/algafood/domain/model/Produto.java
+++ src/main/java/com/algaworks/algafood/domain/model/Produto.java
@@ -2,18 +2,13 @@ package com.algaworks.algafood.domain.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -27,26 +22,28 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Restaurante {
+public class Produto {
 	
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(nullable = false) //detalhe fisico da coluna, apenas para a criação da tabela
+	@Column(nullable = false)
 	private String nome;
 	
-	@Column(name = "taxa_frete", nullable = false)
-	private BigDecimal taxaFrete;
+	@Column(nullable = false)
+	private String descricao;
+	
+	@Column(nullable = false)
+	private BigDecimal preco;
+	
+	@Column(nullable = false)
+	private Boolean ativo;
 	
 	@ManyToOne
-	@JoinColumn(name = "cozinha_id", nullable = false)
-	private Cozinha cozinha;
-	
-	@JsonIgnore
-	@Embedded
-	private Endereco endereco;
+	@JoinColumn(nullable = false)
+	private Restaurante restaurante;
 	
 	@JsonIgnore
 	@CreationTimestamp
@@ -57,18 +54,5 @@ public class Restaurante {
 	@UpdateTimestamp
 	@Column(nullable = false, columnDefinition = "datetime")
 	private LocalDateTime dataAtualizacao;
-	
-	@JsonIgnore
-	@ManyToMany
-	@JoinTable(name = "restaurante_forma_pagamento", 
-	joinColumns = @JoinColumn(name = "restaurante_id"), 
-	inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
-	private List<FormaPagamento> formasPagamento = new ArrayList<>();
-
-
-
-	
-	
-	
 	
 }
