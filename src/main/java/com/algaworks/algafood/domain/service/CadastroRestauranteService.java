8cac496f0da66dc5c2a1424d3d8b58caa96f8794
lucas <lucascanellasdev@gmail.com>
2022-12-03 23:29:29 -0300

    8.11. Desafio: lançando exceptions de granularidade fina

--- src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -3,15 +3,13 @@ package com.algaworks.algafood.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.RestauranteNaoEncontradoException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 
 @Service
 public class CadastroRestauranteService {
-	
-	private static final String MSG_RESTAURANTE_NAO_ENCONTRADO = "Não existe restautante cadastrado com o id %d";
 
 	@Autowired
 	private RestauranteRepository restauranteRepository;
@@ -30,7 +28,7 @@ public class CadastroRestauranteService {
 	}
 	
 	public Restaurante buscarOuFalhar(Long restauranteId) {
-		return restauranteRepository.findById(restauranteId).orElseThrow(() -> new EntidadeNaoEncontradaException(String.format(MSG_RESTAURANTE_NAO_ENCONTRADO, restauranteId)));
+		return restauranteRepository.findById(restauranteId).orElseThrow(() -> new RestauranteNaoEncontradoException(restauranteId));
 	}
 	
 }
