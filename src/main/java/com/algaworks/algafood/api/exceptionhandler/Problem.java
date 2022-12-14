8fa1802bb0a62fe5e1fe41b386c13f131b9d2100
lucas <lucascanellasdev@gmail.com>
2022-12-14 20:07:22 -0300

    8.29. Desafio: estendendo o formato do problema

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
+import java.time.LocalDateTime;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
@@ -14,6 +16,8 @@ public class Problem {
 	private Integer status;
 	private String type;
 	private String title;
-	private String detail;
+	private String detail;	
+	private String userMessage;
+	private LocalDateTime timestamp;
 	
 }
