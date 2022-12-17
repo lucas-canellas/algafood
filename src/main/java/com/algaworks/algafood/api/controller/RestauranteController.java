9fe772d3a28297b58c87968f6eda44024521ff2c
lucas <lucascanellasdev@gmail.com>
2022-12-17 11:01:39 -0300

    9.8. Convertendo grupos de constraints para validação em cascata com @ConvertGroup

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -57,7 +57,7 @@ public class RestauranteController {
 
 	@PostMapping
 	@ResponseStatus(value = HttpStatus.CREATED)
-	public Restaurante adicionar(@RequestBody @Validated(Groups.CadastroRestaurante.class) Restaurante restaurante) {		
+	public Restaurante adicionar(@RequestBody @Valid Restaurante restaurante) {		
 		try {
 			return cadastroRestaurante.salvar(restaurante);
 		} catch (EntidadeNaoEncontradaException e) {
