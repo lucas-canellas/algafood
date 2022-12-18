f96f5792f672a5224d6f7c7f73c1faace437ea89
lucas <lucascanellasdev@gmail.com>
2022-12-18 12:01:03 -0300

    10.12. Voltando o estado inicial do banco de dados para cada execução de teste com callback do Flyway

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,7 +1,10 @@
 package com.algaworks.algafood;
 
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -15,11 +18,21 @@ public class CadastroCozinhaIT {
 	@LocalServerPort
 	private int port;
 	
+	@Autowired
+	private Flyway flyway;
+	
+	@BeforeEach
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
+		
+		flyway.migrate();
+	}
+	
 	@Test
 	public void deveRetornarStatus200_QuadoConsultarCozinha() {
 		RestAssured.given()
-			.basePath("/cozinhas")
-			.port(port)
 			.accept(ContentType.JSON)
 		.when()
 			.get()
@@ -28,18 +41,26 @@ public class CadastroCozinhaIT {
 	}
 	
 	@Test
-	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-		
+	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {		
 		RestAssured.given()
-			.basePath("/cozinhas")
-			.port(port)
 			.accept(ContentType.JSON)
 		.when()
 			.get()
 		.then()
 			.body("", Matchers.hasSize(4));
 //			.body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
+	}
+	
+	@Test
+	public void testRetornarStatus201_QuandoCadastrarCozinha() {
+		RestAssured.given()
+			.body("{ \"nome\": \"Chinesa\" }")
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
 	}
 	
 }
