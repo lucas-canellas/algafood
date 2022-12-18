9ac7bcd277fbc355535533b3d7ec76dba772e3ae
lucas <lucascanellasdev@gmail.com>
2022-12-17 22:07:57 -0300

    10.2. Preparando o projeto para testes de integração

--- src/test/java/com/algaworks/AlgafoodApiApplicationTests.java
+++ src/test/java/com/algaworks/AlgafoodApiApplicationTests.java
@@ -1,13 +1,16 @@
 package com.algaworks;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AlgafoodApiApplicationTests {
+@SpringBootTest(classes= {AlgafoodApiApplicationTests.class})
+public class AlgafoodApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertFalse(true);
 	}
 
 }
